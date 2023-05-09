7598334d51b24dad3cda305396129d5214ad3c8a
tree <treeanago1140@gmail.com>
2023-05-09 15:46:53 +0900

    checkaccess update

--- HasAccessProcess.java
+++ HasAccessProcess.java
@@ -5,25 +5,22 @@ public class HasAccessProcess implements Process{
 
     //状態変更のメソッド
     public void update(String table, String column, String after, String text, String value){
-        
+        DB.updateData(table, column, after, text, value);
     }
 
     //カラム追加のメソッド
     public void addColumn(String title, String state){
-        Connection con = DB.getCon();
     }
 
     //カラム削除のメソッド
     public void deleteColumn(String title){
-        Connection con = DB.getCon();
     }
 
     /**
      * 作品一覧を返すメソッド
      */
     public ArrayList<String> getList(String state){
-        ArrayList<String> list = new ArrayList<>();
-        return list;
+        return DB.getData();
     }
 
 }
