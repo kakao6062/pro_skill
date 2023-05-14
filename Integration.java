8d8eebf50a53989d1045cb19d5d64f83707dcae2
kakao <masazumi6062@icloud.com>
2023-05-14 22:00:53 +0900

    integration更新

--- Integration.java
+++ Integration.java
@@ -4,13 +4,13 @@ class Integration {
     private String name;
     private String pass;
     private ProcessPattern pp = null;
+    private Scanner sc = new Scanner(System.in);
 
     public void process() {
         int select = 100;
         DB.createTable("user");
-        Scanner sc = new Scanner(System.in);
 
-        while (select != 0) {
+        while (select != -1) {
             System.out.println("行いたい処理を入力");
             System.out.println("1: 登録");
             System.out.println("2: ログイン");
@@ -20,8 +20,10 @@ class Integration {
             select = sc.nextInt();
 
             switch (select) {
-                case 1:registrationUser();  break;
-                case 2:login();             break;
+                case 1: registrationUser();
+                        select = -1;        break;
+                case 2: login();
+                        select = -1;        break;
                 case 0:return;
                 default:System.out.println("入力が不適です．再入力してください．");
                     break;
@@ -30,53 +32,66 @@ class Integration {
 
         select = 100;
 
-        while(select != 0) {
+        while(select != -1) {
             System.out.println("行いたい処理を入力");
-            System.out.println("1: リストの更新");
-            System.out.println("2: リストの表示");
-            System.out.println("3: 視聴状況の更新");
-            System.out.println("4: パスワードの更新");
+            System.out.println("1: リストの追加");
+            System.out.println("2: リストの更新");
+            System.out.println("3: リストの表示");
+            System.out.println("4: 視聴状況の更新");
+            System.out.println("5: パスワードの更新");
             System.out.println("0: 終了");
-            System.out.println("> ");
+            System.out.print("> ");
             select = sc.nextInt();
 
             switch (select) {
-                case 1:update();    break;
-                case 2:getList();   break;
-                case 3:update();    break;
-                case 4:setPass();   break;
+                case 1: add();      break;
+                case 2: update();   break;
+                case 3: getList();  break;
+                case 4: update();   break;
+                case 5: setPass();  break;
+                case 0: return;
                 default:System.out.println("入力が不適です．再入力してください．");
-                    break;
+                        break;
             }
         }
     }
 
     private void registrationUser() {
-        Scanner sc = new Scanner(System.in);
+        int error = -1;
+        
         System.out.print("名前を入力 > ");
         name = sc.next();
         System.out.print("パスワードを入力 > ");
         pass = sc.next();
 
         ArrayList<String> data = new ArrayList<>(Arrays.asList(name, pass));
-        DB.insertRow("user", data);
-        DB.createTable(name);
+        error = DB.insertRow("user", data);
 
-        System.out.println("登録完了");
+        if (error == 0) {
+            DB.createTable(name);
+            check(name, pass);
+            System.out.println("登録完了");
+        }else{
+            System.out.println("この名前は既に使用されてます．異なる名前にしてください．");
+            registrationUser();
+        }        
     }
 
     private void login(){
-        Scanner sc = new Scanner(System.in);
         System.out.print("名前を入力 > ");
         name = sc.next();
         System.out.print("パスワードを入力 > ");
         pass = sc.next();
-        check(name, pass);
-
-        System.out.println("ログイン完了");
+        if(check(name, pass)){
+            System.out.println("ログイン完了");
+            check(name, pass);
+        }else{
+            System.out.println("パスワードエラー");
+            login();
+        }
     }
 
-    private void check(String username, String pass){
+    private boolean check(String username, String pass){
         ArrayList<String[]> data = DB.getData("user", "name", username);
         if (pp == null) {
             pp = new ProcessPattern();
@@ -85,8 +100,26 @@ class Integration {
         int passwd_idx = 2;
         if(pass.equals(data.get(0)[passwd_idx])){
             pp.changeToAccess();
+            return true;
         }else{
             pp.chageToNoAccess();
+            return false;
+        }
+    }
+
+    private void add() {
+        ArrayList<String> data = new ArrayList<>();
+        System.out.print("作品名を入力 > ");
+        data.add(sc.next());
+        System.out.print("ジャンルを入力 > ");
+        data.add(sc.next());
+        System.out.print("状態を入力 > ");
+        data.add(sc.next());
+
+        int error = pp.insertRow(name, data);
+
+        if (error != 0) {
+            System.out.println("すでにその作品があるか，アクセス権がありません．");
         }
     }
 
@@ -94,24 +127,20 @@ class Integration {
      * 視聴状況の更新
      */
     private void update(){
-        Scanner sc = new Scanner(System.in);
         check(name, pass);
         System.out.print("どの作品について更新するかを入力 > ");
         String key = sc.next();
         System.out.print("変更後の状態を入力 > ");
         String state = sc.next();
-        String id = pp.getData("user", "name", name).get(0)[0];
-        pp.update(id, key, state);
+        pp.update(name, key, state);
     }
 
     /**
      * get works list that entered state
      */
     private void getList(){
-        Scanner sc = new Scanner(System.in);
         check(name, pass);
-        String id = pp.getData("user", "name", name).get(0)[0];
-        System.out.println("取得したいリストの種類を選択\n1: ジャンル\n2: 視聴状況");
+        System.out.print("取得したいリストの種類を選択\n1: ジャンル\n2: 視聴状況\n> ");
         int choice = sc.nextInt();
         String column;
         if(choice == 1){
@@ -119,16 +148,15 @@ class Integration {
         } else {
             column = "state";
         }
-        System.out.println("対象を入力");
+        System.out.print("作品名を入力 > ");
         String key = sc.next();
-        ArrayList<String[]> list = pp.getData(id, column, key);
+        ArrayList<String[]> list = pp.getData(name, column, key);
         for(String[] s : list){
             System.out.println(String.format("%15s %s", s[0], s[choice]));
         }
     }
 
     private void setPass(){
-        Scanner sc = new Scanner(System.in);
         check(name, pass);
         System.out.print("更新後のパスワードを入力 > ");
         String after = sc.next();
--- IntegrationTest.java
+++ IntegrationTest.java
@@ -1,6 +1,5 @@
 public class IntegrationTest {
     public static void main(String[] args) {
-        DB.deleteTable("user");
         Integration ig = new Integration();
         ig.process();
     }
